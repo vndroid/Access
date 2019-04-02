ea1acf61987a17443a7ec4046a770337ec2b0444
vndroid <diracsvip@gmail.com>
2019-04-02 14:16:42 +0800

    Update
    - Add Qiyu Browser support

--- Access_UA.php
+++ Access_UA.php
@@ -263,6 +263,10 @@ class Access_UA {
 				$this->browserID = 'QQBrowser';
 				$this->browserName = 'QQ浏览器';
 				$this->browserVersion = $matches[1];
+			} elseif (preg_match('#Qiyu/([a-zA-Z0-9.]+)#i', $this->ua, $matches)) {
+				$this->browserID = 'Qiyu';
+				$this->browserName = '旗鱼浏览器';
+				$this->browserVersion = $matches[1];
 			} elseif (preg_match('#UCWEB([a-zA-Z0-9.]+)#i', $this->ua, $matches)) {
 				$this->browserID = $this->browserName = 'UCWEB';
 				$this->browserVersion = $matches[1];
