069ecab65470a2737a1b57b32cf43cd3b57b09bd
vndroid <diracsvip@gmail.com>
2019-04-05 14:22:02 +0800

    Update
    - Update the UA detection regular expression

--- Plugin.php
+++ Plugin.php
@@ -5,7 +5,7 @@ require_once __DIR__ . '/Access_Bootstrap.php';
  *
  * @package Access
  * @author Kokororin
- * @version 2.0.5
+ * @version 2.0.6
  * @link https://github.com/Vndroid/Access
  */
 class Access_Plugin implements Typecho_Plugin_Interface
