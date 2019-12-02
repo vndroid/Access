393be063166aadabfe08952048bee26767877abd
Kane <diracsvip@gmail.com>
2019-12-02 12:34:54 +0800

    Add Firefox Focus Browser Support And Update README.md

--- Plugin.php
+++ Plugin.php
@@ -5,7 +5,7 @@ require_once __DIR__ . '/Access_Bootstrap.php';
  *
  * @package Access
  * @author Kokororin
- * @version 2.2.1
+ * @version 2.2.2
  * @link https://github.com/Vndroid/Access
  */
 class Access_Plugin implements Typecho_Plugin_Interface
