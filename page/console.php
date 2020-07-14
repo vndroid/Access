564b71ff0adb2dbece8b4938b44ce1d0585c6e68
vndroid <diracsvip@gmail.com>
2020-07-14 11:53:04 +0800

    Update
    - Fix Chromium Style Issues

--- page/console.php
+++ page/console.php
@@ -5,6 +5,7 @@ include 'menu.php';
 require_once __DIR__ . '/../Access_Bootstrap.php';
 $access = new Access_Core();
 ?>
+<link rel="stylesheet" href="<?php $options->pluginUrl('Access/css/style.css?v20200714')?>">
 <link rel="stylesheet" href="<?php $options->pluginUrl('Access/sweetalert/sweetalert.css')?>">
 <div class="main">
     <div class="body container">
